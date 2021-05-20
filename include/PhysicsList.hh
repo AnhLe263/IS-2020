5070f09a85b08b4120bcd16871e0b40f728f53f9
LE TUAN ANH <letuananh.nuclphys@gmail.com>
2021-05-20 14:29:13 +0700

    add physics

--- include/PhysicsList.hh
+++ include/PhysicsList.hh
@@ -1,15 +1,28 @@
-#ifndef Anh_Phys
-#define Anh_Phys
+#ifndef PhysicsList_h
+#define PhysicsList_h 1
 
-#include "G4VUserPhysicsList.hh"
+#include "G4VModularPhysicsList.hh"
+#include "globals.hh"
+#include "PhysicsListMessenger.hh"
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-class PhysicsList: public G4VUserPhysicsList
+class PhysicsList: public G4VModularPhysicsList
 {
 public:
   PhysicsList();
-  ~PhysicsList();
-protected:
-  void ConstructParticle();
-  void ConstructProcess();
+ ~PhysicsList();
+
+public:
+  virtual void ConstructParticle();
+  virtual void ConstructProcess();
+  virtual void SetCuts();
+  void AddPhysicsList(const G4String& name);
+  G4VPhysicsConstructor*        fEmPhysicsList; 
+  G4String                      fEmName;
+  PhysicsListMessenger *fMessenger;
+  void AddDecay();
 };
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
 #endif
--- src/PhysicsList.cc
+++ src/PhysicsList.cc
@@ -1,129 +1,179 @@
 #include "PhysicsList.hh"
 
-#include "G4PhysicsListHelper.hh"
-#include "G4ParticleDefinition.hh"
-#include "G4SystemOfUnits.hh"
-#include "G4PhotoNuclearCrossSection.hh"
-#include "talyscrosssection.hh"
-#include "custommodel.hh"
-#include "G4GammaNuclAngDst.hh"
-PhysicsList::PhysicsList() : G4VUserPhysicsList()
-{}
+#include "G4EmStandardPhysics.hh"
+#include "G4EmStandardPhysics_option1.hh"
+#include "G4EmStandardPhysics_option2.hh"
+#include "G4EmStandardPhysics_option3.hh"
+#include "G4EmStandardPhysics_option4.hh"
+#include "G4EmStandardPhysicsWVI.hh"
+#include "G4EmStandardPhysicsGS.hh"
+#include "G4EmStandardPhysicsSS.hh"
+#include "G4EmLivermorePhysics.hh"
+#include "G4EmPenelopePhysics.hh"
+#include "G4EmLowEPPhysics.hh"
+#include "G4EmParameters.hh"
+#include "GammaNuclearPhysics.hh"
+PhysicsList::PhysicsList(): G4VModularPhysicsList()
+{
+    fMessenger = new PhysicsListMessenger(this);
+    fEmPhysicsList = new G4EmStandardPhysics();
+}
 
-PhysicsList::~PhysicsList() {}
+PhysicsList::~PhysicsList()
+{
 
-// construct particle
+}
 #include "G4BosonConstructor.hh"
 #include "G4LeptonConstructor.hh"
 #include "G4MesonConstructor.hh"
+#include "G4BosonConstructor.hh"
 #include "G4BaryonConstructor.hh"
 #include "G4IonConstructor.hh"
 #include "G4ShortLivedConstructor.hh"
-
 void PhysicsList::ConstructParticle()
 {
-  G4BosonConstructor bosonConst;
-  bosonConst.ConstructParticle();
 
-  G4LeptonConstructor leptonConst;
-  leptonConst.ConstructParticle();
+    G4BosonConstructor  pBosonConstructor;
+    pBosonConstructor.ConstructParticle();
 
-  G4MesonConstructor mesonConst;
-  mesonConst.ConstructParticle();
+    G4LeptonConstructor pLeptonConstructor;
+    pLeptonConstructor.ConstructParticle();
 
-  G4BaryonConstructor baryonConst;
-  baryonConst.ConstructParticle();
+    G4MesonConstructor pMesonConstructor;
+    pMesonConstructor.ConstructParticle();
 
-  G4IonConstructor ionConst;
-  ionConst.ConstructParticle();
+    G4BaryonConstructor pBaryonConstructor;
+    pBaryonConstructor.ConstructParticle();
 
-  G4ShortLivedConstructor shortLivedConst;
-  shortLivedConst.ConstructParticle();
+    G4IonConstructor pIonConstructor;
+    pIonConstructor.ConstructParticle();
+
+    G4ShortLivedConstructor pShortLivedConstructor;
+    pShortLivedConstructor.ConstructParticle(); 
 }
 
-// Processes
-// General
-#include "G4Decay.hh"
-#include "G4RadioactiveDecay.hh"
-//for gamma
-#include "G4ComptonScattering.hh"
-#include "G4PhotoElectricEffect.hh"
-#include "G4GammaConversion.hh"
-//for e- and e+
-#include "G4eIonisation.hh"
-#include "G4eMultipleScattering.hh"
-#include "G4eBremsstrahlung.hh"
-#include "G4eplusAnnihilation.hh"
 
-#include "G4PhotoNuclearProcess.hh"
-#include "G4CascadeInterface.hh"
-#include "G4BinaryCascade.hh"
-#include "G4CascadeParameters.hh"
-#include "G4LENDorBERTModel.hh"
-
-#include "G4hMultipleScattering.hh"
-#include "G4ionIonisation.hh"
-#include "G4IonParametrisedLossModel.hh"
-#include "G4NuclearStopping.hh"
-#include "customprocess.hh"
 void PhysicsList::ConstructProcess()
 {
-  AddTransportation();
-
-  G4PhysicsListHelper* ph =G4PhysicsListHelper::GetPhysicsListHelper();
+    // transportation
+    //
+    AddTransportation();
   
-  GetParticleIterator()->reset();
-  while ((*GetParticleIterator())())
-    {
-      G4ParticleDefinition* particle=GetParticleIterator()->value();
-      G4String partName=particle->GetParticleName();
-      G4Decay *aDecay=new G4Decay();
-      if (aDecay->IsApplicable(*particle)) ph->RegisterProcess(aDecay,particle);
-      G4RadioactiveDecay* aRadioactiveDecay=new G4RadioactiveDecay();
-      aRadioactiveDecay->SetARM(false);
-      ph->RegisterProcess(aRadioactiveDecay,particle);
-      if (partName == "gamma")
-	{
-	  ph->RegisterProcess(new G4ComptonScattering(),particle);
-	  ph->RegisterProcess(new G4PhotoElectricEffect(),particle);
-	  ph->RegisterProcess(new G4GammaConversion(),particle);
-      G4PhotoNuclearProcess* InPro = new G4PhotoNuclearProcess;
-      //customprocess* InPro = new customprocess;
-      InPro->BiasCrossSectionByFactor(100.);
-      //G4CascadeInterface* Bert=new G4CascadeInterface;
-      //Bert->usePreCompoundDeexcitation();
-      //Bert->useCascadeDeexcitation();
-      //InPro->RegisterMe(Bert);
-      //InPro->RegisterMe(new G4BinaryCascade);
-      //G4LENDorBERTModel *lend=new G4LENDorBERTModel(particle);
-      //lend->SetMinEnergy(0*keV);
-      //lend->SetMaxEnergy(500*MeV);
-      //InPro->RegisterMe(lend);
-      talyscrosssection* Xs=new talyscrosssection;
-      InPro->AddDataSet(Xs);
-      custommodel* customModel=new custommodel;
-      //InPro->RegisterMe(customModel);
-      //ph->RegisterProcess(InPro,particle);
-	}
-      else if (partName == "e-" || partName == "e+")
-	{
-	  ph->RegisterProcess(new G4eMultipleScattering(),particle);
-	  ph->RegisterProcess(new G4eBremsstrahlung(),particle);
-	  ph->RegisterProcess(new G4eIonisation(),particle);
-	  if (partName == "e+")
-	    ph->RegisterProcess(new G4eplusAnnihilation(),particle);
-	}
-      else if (partName=="GenericIon")
-            {
+    // electromagnetic physics list
+    //
+    fEmPhysicsList->ConstructProcess();
+    // Gamma
+    GammaNuclearPhysics* gammaN=new GammaNuclearPhysics();
+    gammaN->ConstructProcess();
+    // decay process
 
-       ph->RegisterProcess(new G4hMultipleScattering(),particle);
-                G4ionIonisation* ionIon=new G4ionIonisation();
-       ionIon->SetEmModel(new G4IonParametrisedLossModel());
-       ph->RegisterProcess(ionIon,particle);
-       ph->RegisterProcess(new G4NuclearStopping,particle);
-
-      }
-    }
+    //
+    AddDecay();
 }
 
 
+void PhysicsList::AddPhysicsList(const G4String& name)
+{
+    if (verboseLevel>-1) {
+        G4cout << "PhysicsList::AddPhysicsList: <" << name << ">" << G4endl;
+    }
+
+    if (name == "emstandard_opt0") {
+
+    fEmName = name;
+    delete fEmPhysicsList;
+    fEmPhysicsList = new G4EmStandardPhysics();
+    
+    } else if (name == "emstandard_opt1") {
+
+    fEmName = name;
+    delete fEmPhysicsList;
+    fEmPhysicsList = new G4EmStandardPhysics_option1();
+
+    } else if (name == "emstandard_opt2") {
+
+    fEmName = name;
+    delete fEmPhysicsList;
+    fEmPhysicsList = new G4EmStandardPhysics_option2();
+        
+    }  else if (name == "emstandard_opt3") {
+
+    fEmName = name;
+    delete fEmPhysicsList;
+    fEmPhysicsList = new G4EmStandardPhysics_option3();
+    
+    } else if (name == "emstandard_opt4") {
+
+    fEmName = name;
+    delete fEmPhysicsList;
+    fEmPhysicsList = new G4EmStandardPhysics_option4();
+    
+    } else if (name == "emstandardSS") {
+
+    fEmName = name;
+    delete fEmPhysicsList;
+    fEmPhysicsList = new G4EmStandardPhysicsSS();
+
+    } else if (name == "emstandardWVI") {
+
+    fEmName = name;
+    delete fEmPhysicsList;
+    fEmPhysicsList = new G4EmStandardPhysicsWVI();
+
+    } else if (name == "standardGS") {
+
+    fEmName = name;
+    delete fEmPhysicsList;
+    fEmPhysicsList = new G4EmStandardPhysicsGS();
+                    
+    } else if (name == "emlivermore") {
+    fEmName = name;
+    delete fEmPhysicsList;
+    fEmPhysicsList = new G4EmLivermorePhysics();
+    
+    } else if (name == "empenelope") {
+    fEmName = name;
+    delete fEmPhysicsList;
+    fEmPhysicsList = new G4EmPenelopePhysics();
+
+    } else if (name == "emlowenergy") {
+    fEmName = name;
+    delete fEmPhysicsList;
+    fEmPhysicsList = new G4EmLowEPPhysics();
+
+    } else {
+
+    G4cout << "PhysicsList::AddPhysicsList: <" << name << ">"
+           << " is not defined"
+           << G4endl;
+    }
+    G4EmParameters::Instance()->SetBuildCSDARange(true);
+}
+
+
+void PhysicsList::SetCuts()
+{
+    //SetDefaultCutValue();
+}
+
+#include "G4GenericIon.hh"
+#include "G4Radioactivation.hh"
+void PhysicsList::AddDecay()
+{
+  // decay process
+  //
+  //G4Decay* fDecayProcess = new G4Decay();
+ G4PhysicsListHelper* ph =G4PhysicsListHelper::GetPhysicsListHelper();
+  auto particleIterator=GetParticleIterator();
+  particleIterator->reset();
+  while( (*particleIterator)() ){
+    G4ParticleDefinition* particle = particleIterator->value();
+     G4String partName=particle->GetParticleName();
+      if (partName=="GenericIon")
+	{
+	  G4Radioactivation* radioactiveDecay = new G4Radioactivation();
+	  radioactiveDecay->SetARM(true);               //Atomic Rearangement
+	  ph->RegisterProcess(radioactiveDecay, G4GenericIon::GenericIon());
+	}
+  }
+}
